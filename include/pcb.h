5408cb67722f1e295ccf7f2caab2ca4543eb65b5
VishalDhanasekaran <2005vishaldhanasekaran@gmail.com>
2025-02-02 23:43:03 +0530

    kernel skeleton

---
+++ include/pcb.h
@@ -0,0 +1,27 @@
+
+// === include/pcb.h ===
+#ifndef PCB_H
+#define PCB_H
+#include <stdint.h>
+
+typedef enum 
+{
+    READY,
+    RUNNING,
+    BLOCKED,
+    TERMINATED
+} TaskState;
+
+typedef struct PCB 
+{
+    uint32_t pid;
+    char *pname;
+    uint32_t priority;
+    TaskState state;
+    uint32_t burst_time;
+    uint32_t *stack;
+    uint32_t stack_size;
+} PCB;
+
+#endif // PCB_H
+
---
+++ include/queue.h
@@ -0,0 +1,26 @@
+// === include/queue.h ===
+#ifndef QUEUE_H
+#define QUEUE_H
+#include <stdbool.h>
+#include "pcb.h"
+
+#define MAXQSIZE 10
+
+typedef struct Queue 
+{
+    int head;
+    int tail;
+    PCB *queue[MAXQSIZE];
+    int size;
+} Queue;
+
+void initializeQueue(Queue *q);
+bool isFull(Queue *q);
+bool isEmpty(Queue *q);
+void enqueue(Queue *q, PCB *newTask);
+void dequeue(Queue *q);
+PCB *front(Queue *q);
+
+#endif // QUEUE_H
+
+
