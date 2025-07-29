428e3cddb85e4aa6bd133d90bc6d65c30d8de9cf
VishalDhanasekaran <2005vishaldhanasekaran@gmail.com>
2025-07-30 00:37:45 +0530

    feat: Functionality to write on serial port

---
+++ include/serial.h
@@ -0,0 +1,10 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+void serial_init();
+void serial_writechar(char c);
+void serial_writestring(const char *str);
+void serial_writeint(int num);
+void serial_writehex(uint32_t num);
+
+#endif
