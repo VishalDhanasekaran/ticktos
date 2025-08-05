b8e8879af17c0f2bd230d0beaa29b9fdd1631291
VishalDhanasekaran <2005vishaldhanasekaran@gmail.com>
2025-08-05 23:22:40 +0530

    feat(headerfiles): timer, utils

---
+++ include/utils.h
@@ -0,0 +1,18 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define ALIGNUP(offset, align) ((offset) + ((align) + 1) & ~((align)-1))
+
+extern unintptr_t heap_start, heap_end, kalloc_ptr;
+extern size_t heap_size;
+
+typedef struct memory_blocks
+{
+    uint64_t start;
+    uint64_t end;
+} __attribute__((packed)) memory_blocks_t;
+
+#endif
